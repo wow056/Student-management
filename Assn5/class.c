93e8ef1c24fb3da14480f334e6dd119d4ab7a75d
wow056 <powerful@postech.ac.kr>
2018-05-22 04:24:42 +0900

    이중 연결 리스트 사용 정렬 함수 추가(미완성)

--- Assn5/class.c
+++ Assn5/class.c
@@ -2,37 +2,121 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void init(LIST *plist)
-{
+void init(LIST *plist){
 	plist->head = NULL;
 	plist->count = 0;
 }
 
-void show(const LIST *plist)
-{
+void show(const LIST *plist){
 	NODE *ptr;
 	printf("NAME\t\tGender\tHeight\tEmail\n");
 	for (ptr = plist->head; ptr != NULL; ptr = ptr->next)
-		printf("%s\t%c\t%d\t%s\n", ptr->data.email, ptr->data.gender, ptr->data.height, ptr->data.name);
+		printf("%s\t\t%c\t%d\t%s\n", ptr->data.name, ptr->data.gender, ptr->data.height, ptr->data.email);
 	
 }
 
-void add(LIST *plist, STUDENT data)
-{
+void add(LIST *plist, STUDENT data){
 	NODE *temp;
 	temp = (NODE*)malloc(sizeof(NODE));
+	temp->prev = NULL;
 	temp->data = data;
 	temp->next = plist->head;
+	plist->head->prev = temp;
 	plist->head = temp;
 	plist->count++;
 }
 
-void remove(LIST *plist)
-{
+void remove(NODE
+
+void del(LIST *plist){
 	NODE *temp;
 	temp = plist->head;
-	plist->head = plist->head->next;
+	plist->head = temp->next;
+	plist->head->prev = NULL;
 	plist->count--;
 	free(temp);
 }
 
+void swap(NODE *a, NODE *b) {
+	NODE temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void insert(LIST *plist, NODE *destination, NODE *pick) {
+	if (pick == plist->head) {
+
+	}
+	
+	pick->prev->next = pick->next;
+	pick->next->prev = pick->prev;
+	destination->prev = pick;
+	pick->next = destination;
+}
+
+void insertion_sort(LIST *plist){
+	NODE *ptr, *pick, *mark;
+	for (pick = plist->head; pick != NULL; pick = ptr->next) {
+		for (ptr = plist->head; ptr != pick; ptr = ptr->next) {
+			if (pick->data.height > ptr->data.height) {
+				insert(ptr, pick);
+			}
+		}
+	}
+}
+
+STUDENT pop(LIST *plist) {
+	STUDENT temp;
+	temp = plist->head->data;
+	del(plist);
+	return temp;
+}
+
+void sort(LIST *plist) {
+	LIST m_list, f_list;
+	STUDENT temp;
+	int i, n;
+
+	init(&m_list);
+	init(&f_list);
+
+	while (plist->head != NULL) {
+		temp = pop(plist);
+		if (temp.gender == 'm') {
+			add(&m_list, temp);
+		}
+		else if (temp.gender == 'f') {
+			add(&f_list, temp);
+		}
+	}
+
+	insertion_sort(&m_list);
+	insertion_sort(&f_list);
+
+	if (m_list.count > f_list.count) {
+		while (m_list.count == f_list.count) {
+			add(plist, pop(&m_list));
+		}
+	}
+	else if (m_list.count < f_list.count) {
+		while (m_list.count == f_list.count) {
+			add(plist, pop(&f_list));
+		}
+	}
+
+	if (m_list.head->data.height >= f_list.head->data.height) {
+		n = m_list.count;
+		for (i = 0; i < n; i++) {
+			add(plist, pop(&m_list));
+			add(plist, pop(&f_list));
+		}
+	}
+	else {
+		n = m_list.count;
+		for (i = 0; i < n; i++) {
+			add(plist, pop(&f_list));
+			add(plist, pop(&m_list));
+		}
+	}
+}
--- Assn5/class.h
+++ Assn5/class.h
@@ -5,10 +5,12 @@ typedef struct student {
 	char name[20];
 	char gender;
 	int height; //positive intege[cm]
+	int score;
 	char email[30]; //identifier
 } STUDENT;
 
 typedef struct node {
+	struct node *prev;
 	struct student data;
 	struct node *next;
 } NODE;
@@ -18,4 +20,12 @@ typedef struct list {
 	int count;
 } LIST;
 
+
+void init(LIST *plist);
+void show(const LIST *plist);
+void add(LIST *plist, STUDENT data);
+void del(LIST *plist);
+void swap(NODE *a, NODE *b);
+void insert(NODE *destination, NODE *pick);
+void insertion_sort(LIST *plist);
 #endif
